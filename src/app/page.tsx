a1ecbe3570e31357e3e0d4a48e329cd783a0eacc
celines <celina.alsina@gmail.com>
2023-12-18 15:53:53 +0100

    Fixed links position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <ul>
-      <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
-        <Link href="/A">Assignment A</Link>
-      </li>
-      <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
-        <Link href="/B">Assignment B</Link>
-      </li>
-      <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
-        <Link href="/C">Assignment C</Link>
-      </li>
-    </ul>
+    <div className="p-4 sm:ml-64">
+      <ul className=" flex flex-col w-full items-center justify-center m-auto p-20">
+        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+          <Link href="/A">Assignment A</Link>
+        </li>
+        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+          <Link href="/B">Assignment B</Link>
+        </li>
+        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+          <Link href="/C">Assignment C</Link>
+        </li>
+      </ul>
+    </div>
+
   );
 }
