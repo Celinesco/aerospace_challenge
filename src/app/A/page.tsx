5003935f39d61f99b394b458c6716613e187c6ef
celines <celina.alsina@gmail.com>
2023-12-18 10:32:40 +0100

    Added tracker component

--- src/app/A/page.tsx
+++ src/app/A/page.tsx
@@ -1,46 +1,62 @@
+'use client';
+import { useState } from "react";
+import StatusTracker from "../_components_assignments/StatusTracker";
+import { RocketData } from "../../../types";
+
 export default function PageA() {
+    const [data, setData] = useState<RocketData | null>({
+        velocity: 2,
+        temperature: 0,
+        statusMessage: "hola terricolas",
+        isAscending: true,
+        isActionRequired: false,
+    });
+
     return (
         <div className="py-20">
-            <div className="p-4 sm:ml-64">
-                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                    <div className="grid grid-cols-3 gap-4 mb-4">
-                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+            {data &&
+                <div className="p-4 sm:ml-64">
+                    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                        <div className="grid grid-cols-3 gap-4 mb-4">
+                            <div className=" rounded bg-gray-50 dark:bg-gray-800 h-max p-2 ">
+                                <StatusTracker
+                                    name={"Rocket name"}
+                                    status={data?.statusMessage}
+                                />
+                            </div>
+                            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                                <p className="text-2xl text-gray-400 dark:text-gray-500">
+                                    velocity
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                                <p className="text-2xl text-gray-400 dark:text-gray-500">
+                                    temperature
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
                             <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                status
+                                action required
                             </p>
                         </div>
-                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                velocity
-                            </p>
-                        </div>
-                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                temperature
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            action required
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-2 gap-4 mb-4">
-                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                graphs
-                            </p>
-                        </div>
-                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                actions
-                            </p>
+                        <div className="grid grid-cols-2 gap-4 mb-4">
+                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                                <p className="text-2xl text-gray-400 dark:text-gray-500">
+                                    graphs
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                                <p className="text-2xl text-gray-400 dark:text-gray-500">
+                                    actions
+                                </p>
+                            </div>
+
                         </div>
 
                     </div>
-
                 </div>
-            </div>
+            }
         </div>
     )
 }
---
+++ src/app/_components_assignments/StatusTracker.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { Card, Title, Tracker, Flex, Text, Color, Badge } from "@tremor/react";
+
+interface Tracker {
+    color: Color;
+    tooltip: string;
+}
+
+const data: Tracker[] = [
+    //replace with tootletip: statusMessage
+    //color:if action was required red else green
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "rose", tooltip: "Downtime" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "emerald", tooltip: "Operational" },
+    { color: "gray", tooltip: "Maintenance" },
+];
+
+type TrackerProps = {
+    name: string,
+    status: string
+}
+
+const StatusTracker = ({ name = "Rocket Name/id", status }: TrackerProps) => {
+    const date = new Date();
+    const hours = date.getHours();
+    const min = date.getMinutes();
+    const minutes = (min < 10 ? '0' : "") + min
+    const sec = date.getSeconds();
+    const seconds = (sec < 10 ? '0' : "") + sec
+
+
+    return (
+        <Card className="w-96">
+            <Title>{name}</Title>
+            <Text>{`Last status at: ${hours}:${minutes}:${seconds}`}</Text>
+            <p className="w-64 whitespace-normal py-1 bg-blue-200 text-blue-900 p-2 rounded mt-1 text-sm">{status}</p>
+            <Flex justifyContent="end" className="mt-4">
+                <Text>Uptime 92%</Text>
+            </Flex>
+            <Tracker data={data} className="mt-2" />
+        </Card>
+    )
+
+};
+
+
+export default StatusTracker;
