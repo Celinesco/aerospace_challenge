099eec336697b88bd29ea9e34e1886ead90121ce
celines <celina.alsina@gmail.com>
2023-12-19 16:13:13 +0100

    Created functions to generate data for props

--- src/app/_components_assignments/utils/functions.ts
+++ src/app/_components_assignments/utils/functions.ts
@@ -1,5 +1,50 @@
 /** @format */
 
+import { RocketData, RocketDataWS } from "../../../../types";
+
 export function formatTimeUnit(unit: number) {
   return unit < 10 ? "0" + unit : unit.toString();
+}
+
+// divided altitude to have the values closer. I guess the altitude is negative once certain point
+// outside the earth is reached
+export function createsTemVsAltitudeProp(
+  list: RocketData[] | RocketDataWS[],
+  temperatureKey: "temperature" | "Temperature",
+  altitudeKey: "Altitude" | "altitude",
+  divisor: number
+) {
+  const newList = list.map((data: any) => {
+    return {
+      temperature: data[temperatureKey],
+      altitude: data[altitudeKey] / divisor,
+    };
+  });
+  return newList;
+}
+
+export function createsdataGraphVelocityProp(
+  list: RocketData[] | RocketDataWS[],
+  velocityKey: "velocity" | "Velocity"
+) {
+  const newList = list.map((data: any) => {
+    return {
+      velocity: data[velocityKey],
+    };
+  });
+  return newList;
+}
+
+export function createsDataStatus(
+  list: RocketData[] | RocketDataWS[],
+  statusMessageKey: "statusMessage" | "StatusMessage",
+  isActionKey: "isActionRequired" | "IsActionRequired"
+) {
+  const newList = list.map((data: any) => {
+    return {
+      tooltip: data[statusMessageKey],
+      color: data[isActionKey] ? "red" : "emerald",
+    };
+  });
+  return newList;
 }
