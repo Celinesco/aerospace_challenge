7d7d105d7f5184fa2381ecfe43c8e14e5de3ef2f
celines <celina.alsina@gmail.com>
2023-12-19 11:17:56 +0100

    Fixed button alignment

--- src/app/A/page.tsx
+++ src/app/A/page.tsx
@@ -60,7 +60,7 @@ export default function PageA() {
     return (
 
         <div className="p-24 md:p-4 sm:ml-64 flex flex-col items-center justify-center">
-            <div className="w-full justify-start" >
+            <div>
                 <Button
                     className="dark:bg-blue-600 dark:text-white"
                     icon={RefreshIcon}
