912cd03eab17590a58ca97257ca630957c94aa38
celines <celina.alsina@gmail.com>
2023-12-19 18:15:50 +0100

    Corrected description for the tests

--- src/app/__tests__/VelocityCard.test.tsx
+++ src/app/__tests__/VelocityCard.test.tsx
@@ -4,14 +4,14 @@ import { render, screen } from "@testing-library/react";
 import VelocityCard from "../_components_assignments/VelocityCard";
 
 describe("VelocityCard Component", () => {
-    it("Checks component when is ascending", () => {
+    it("Checks component when is ascending and corresponding values", () => {
         render(<VelocityCard velocity={77.5} isAscending={true} altitude={10} />);
         screen.getByText(/Velocity km/i);
         screen.getByText("77.5");
         screen.getByText(/Altitude/i);
         screen.getByText(/Ascending/i);
     });
-    it("Checks component when is descending", () => {
+    it("Checks component when is descending and corresponding values", () => {
         render(
             <VelocityCard velocity={77767675} isAscending={false} altitude={10000} />
         );
