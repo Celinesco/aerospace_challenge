c0edc8549c80a0466d5073bbd2d58448e2330a56
celines <celina.alsina@gmail.com>
2023-12-19 14:26:43 +0100

    Alert only if res is ok

--- src/app/_components_assignments/ActionRequired.tsx
+++ src/app/_components_assignments/ActionRequired.tsx
@@ -1,53 +1,56 @@
-'use client';
+/** @format */
+
+"use client";
 import { Button } from "@tremor/react";
 
-export default function ActionRequired({ action }: { action: boolean }) {
+const shimmer =
+    "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-red-500 before:to-transparent";
 
+export default function ActionRequired({ action }: { action: boolean }) {
     const handleClick = async () => {
         // tried with put, post but both methods are not allowed
-        // left get and make nd make an alert
-
+        // left get and make and make an alert
         try {
-            const res = await fetch(`${process.env.API_URL}${process.env.B_ENDPOINT}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            alert("Problem solved, you saved the planet!")
+            const res = await fetch(
+                `${process.env.API_URL}${process.env.B_ENDPOINT}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
             if (!res.ok) {
-                throw new Error("Something went wrong")
+                throw new Error("Something went wrong");
             }
-            const resJson = await res.json()
-
-            // console.log(resJson)
-
+            alert("Problem solved, you saved the planet!");
         } catch (err) {
-            console.log("ERROR", err)
+            console.log("ERROR", err);
         }
-    }
+    };
 
-    const shimmer =
-        'relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-red-500 before:to-transparent';
 
-    if (action) {
+    if (!action) {
         return (
-            <div className={`${shimmer} relative flex items-center justify-center h-24 mb-4 rounded bg-red-200 dark:bg-red-900`}>
+            <div
+                className={`${shimmer} relative flex items-center justify-center h-36 rounded bg-red-200 dark:bg-red-900`}
+            >
                 <p className="text-2xl text-gray-900 dark:text-white">
                     ⚠️ Action required
                 </p>
                 <Button
-                    onClick={handleClick} className="absolute top-2 right-2 dark:bg-blue-600 dark:text-white">Fix all</Button>
+                    onClick={handleClick}
+                    className="absolute top-2 right-2 dark:bg-blue-600 dark:text-white"
+                >
+                    Fix all
+                </Button>
             </div>
-        )
+        );
     } else {
         return (
             <div className="relative flex items-center justify-center h-24 mb-4 rounded bg-green-100 dark:bg-emerald-500">
-                <p className="text-2xl text-gray-900">
-                    No action is required
-                </p>
+                <p className="text-2xl text-gray-900">No action is required</p>
             </div>
-        )
-
+        );
     }
 }
