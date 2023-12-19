8de62e87ef7a29dec91a11fc53b555502574c537
celines <celina.alsina@gmail.com>
2023-12-19 13:31:53 +0100

    Refactor tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,26 @@
 
 import Link from "next/link";
 
+const homeClassnames = {
+  container: "p-4 sm:ml-64",
+  ulist: "flex flex-col w-full items-center justify-center m-auto p-20",
+  listItem: "mb-3 bg-blue-200 text-blue-900 p-2 rounded",
+};
+
 export default function Home() {
   return (
-    <div className="p-4 sm:ml-64">
-      <ul className=" flex flex-col w-full items-center justify-center m-auto p-20">
-        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+    <div className={homeClassnames.container}>
+      <ul className={homeClassnames.ulist}>
+        <li className={homeClassnames.listItem}>
           <Link href="/A">Assignment A</Link>
         </li>
-        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+        <li className={homeClassnames.listItem}>
           <Link href="/B">Assignment B</Link>
         </li>
-        <li className="mb-3 bg-blue-200 text-blue-900 p-2 rounded">
+        <li className={homeClassnames.listItem}>
           <Link href="/C">Assignment C</Link>
         </li>
       </ul>
     </div>
-
   );
 }
