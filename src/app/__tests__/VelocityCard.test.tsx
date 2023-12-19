18b3b3d61f4db089ddb17b28a411670d235f3c35
celines <celina.alsina@gmail.com>
2023-12-19 17:55:55 +0100

    Created unit tests for VelocityCard and ActionRequired components

---
+++ src/app/__tests__/VelocityCard.test.tsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import { render, screen } from "@testing-library/react";
+import VelocityCard from "../_components_assignments/VelocityCard";
+
+describe("VelocityCard Component", () => {
+    it("Checks component when is ascending", () => {
+        render(<VelocityCard velocity={77.5} isAscending={true} altitude={10} />);
+        screen.getByText(/Velocity km/i);
+        screen.getByText("77.5");
+        screen.getByText(/Altitude/i);
+        screen.getByText(/Ascending/i);
+    });
+    it("Checks component when is descending", () => {
+        render(
+            <VelocityCard velocity={77767675} isAscending={false} altitude={10000} />
+        );
+        screen.getByText(/Velocity km/i);
+        screen.getByText("77767675");
+        screen.getByText(/Altitude/i);
+        screen.getByText("10000");
+        screen.getByText(/Descending/i);
+    });
+});
